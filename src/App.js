f39b3726adf77fa6933b4d9e3fa897da9e1a7eeb
Ashish Arya <62978073+Ashish-Arya-CS@users.noreply.github.com>
2021-03-21 10:36:57 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import "./styles.css";
+import React, { useState, useEffect, Fragment } from "react";
+import states from "./states";
+
+const App = () => {
+  console.log("render");
+  const [search, setSearch] = useState("");
+  const [filteredStates, setFilteredStates] = useState(states);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      const filter = states.filter((state) => {
+        return state.author.toLowerCase().includes(search.toLowerCase());
+      });
+
+      setFilteredStates(filter);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, [search]);
+
+  return (
+    <Fragment>
+      <div>
+        Start Typing The Author Name In The Search Bar And It Will Filter The
+        Details.
+      </div>
+      Search:-
+      <input value={search} onChange={(e) => setSearch(e.target.value)} />
+      <ul>
+        {filteredStates &&
+          filteredStates.map((state) => {
+            return (
+              <li>
+                <ul key={state.author}>Isbn :{state.isbn}</ul>
+                <ul>Title: {state.title}</ul>
+                <ul>Subtitle: {state.subtitle}</ul>
+                <ul>Author: {state.author}</ul>
+                <br></br>
+              </li>
+            );
+          })}
+      </ul>
+    </Fragment>
+  );
+};
+
+export default App;
